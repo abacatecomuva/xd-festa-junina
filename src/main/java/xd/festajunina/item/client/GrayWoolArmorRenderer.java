149cd634e49e84abcb236d918c0166a5b1b1697c
abacatecomuva <abacatecomuva@gmail.com>
2024-07-05 20:21:13 -0300

    gray armor ?

--- src/main/java/xd/festajunina/item/client/GrayWoolArmorRenderer.java
+++ src/main/java/xd/festajunina/item/client/GrayWoolArmorRenderer.java
@@ -8,6 +8,6 @@ import xd.festajunina.item.custom.GrayWoolArmorItem;
 
 public class GrayWoolArmorRenderer extends GeoArmorRenderer<GrayWoolArmorItem> {
     public GrayWoolArmorRenderer() {
-        super(new DefaultedItemGeoModel<>(new Identifier(XDFestaJunina.MOD_ID,"armor/green_twool")));
+        super(new DefaultedItemGeoModel<>(new Identifier(XDFestaJunina.MOD_ID,"armor/gray_twool")));
     }
 }
