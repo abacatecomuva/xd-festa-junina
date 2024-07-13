c614ddbabc1cbfb8c2f8c952de0b7fb44511809c
Andrey Tondo <andreytondo0408@gmail.com>
2024-07-13 12:30:38 -0300

    add dynamic bingo number renderer (not working yet)

--- src/main/java/xd/festajunina/XDFestaJuninaClient.java
+++ src/main/java/xd/festajunina/XDFestaJuninaClient.java
@@ -3,7 +3,10 @@ package xd.festajunina;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import xd.festajunina.item.ModItems;
+import xd.festajunina.item.renderer.BingoNumberItemRenderer;
 import xd.festajunina.screen.BingoCardScreen;
 
 @Environment(EnvType.CLIENT)
@@ -11,6 +14,6 @@ public class XDFestaJuninaClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(XDFestaJunina.BINGO_CARD_SCREEN_HANDLER, BingoCardScreen::new);
-
+        BuiltinItemRendererRegistry.INSTANCE.register(ModItems.BINGO_NUMBER, new BingoNumberItemRenderer());
     }
 }
